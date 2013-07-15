f270e01d9c9910ccb6e9085d8a85268940b6907b
Simon Fletcher <Macbook>
2013-07-15 19:14:38 +0100

    removal of migration data

--- db/migrate/20130309120511_create_users_update.rb
+++
@@ -1,5 +0,0 @@
-class CreateUsersUpdate < ActiveRecord::Migration
-  def change
-  	add_column :users, :access_level, :int
-  end
-end
--- db/migrate/20130310170538_update_admin.rb
+++
@@ -1,8 +0,0 @@
-class UpdateAdmin < ActiveRecord::Migration
-  def up
-  	add_column :admins, :avatar, :string
-  end
-
-  def down
-  end
-end
