76f290c230606c03f7ead2898d6fe297d5107c8b
Simon Fletcher <Macbook>
2013-07-17 23:27:56 +0100

    updates to the file

--- app/controllers/installation/install_controller.rb
+++ app/controllers/installation/install_controller.rb
@@ -7,7 +7,26 @@ class Installation::InstallController < ApplicationController
 
 	def create 
 
-		abort '123123'.inspect
+		remove_unwanted_keys
+
+		params.each do |key, value|
+			set = Setting.where("setting_name = '#{key}'").update_all('setting' => value)
+		end
+
+		redirect_url = "admin_settings_#{redirect}_index_path"
+
+		respond_to do |format|
+			format.html { redirect_to send(redirect_url), notice:  "Settings were updated" }
+		end
+
+	end
+
+	def remove_unwanted_keys
+
+		params.delete :utf8
+		params.delete :authenticity_token
+		params.delete :commit
+		params.delete :redirect
 
 	end
 
