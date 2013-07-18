e9fdfce7aa49688e770f3eedceb613d57f729f62
Simon Fletcher <Macbook>
2013-07-18 20:17:45 +0100

    Installation scripts written now testing

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -6,7 +6,7 @@ class Admin < ActiveRecord::Base
 	
 	validates :email, presence: true, uniqueness: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
 
-  	validates :first_name, :last_name, :username, :access_level, :presence => true 
+  	validates :username, :access_level, :presence => true 
 
 	ACCESS_LEVELS = ["admin", "editor"]
 
