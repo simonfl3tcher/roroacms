e6811a63541abbe8ffe61513c64ef40fdd7027ac
Simon Fletcher <Macbook>
2013-07-18 20:53:12 +0100

    Updates to the db installation section

--- db/migrate/20130718115921_add_initial_page.rb
+++
@@ -1,6 +0,0 @@
-class AddInitialPage < ActiveRecord::Migration
-  def up
-  	Post.create :post_title => 'Welcome to Roroa', :post_type => 'page', :post_slug => 'welcome-to-roroa', :structured_url => '/welcome-to-roroa', :post_date => Time.now.to_s(:db), :admin_id => '1', :post_content => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis in mauris ac ultrices. Morbi interdum varius dictum. Nulla at ullamcorper lacus, non rutrum ante. Proin sed v'
-  	Post.create :post_title => 'First News Post', :post_type => 'post', :post_status => 'Published', :post_slug => 'first-news-post', :post_date => Time.now.to_s(:db), :admin_id => '1', :post_content => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam facilisis in mauris ac ultrices. Morbi interdum varius dictum. Nulla at ullamcorper lacus, non rutrum ante. Proin sed v'
-  end
-end
