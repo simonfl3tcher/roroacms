d9dab0b494985980c91650c666b1a72d0ca07fbf
Simon Fletcher <Macbook>
2013-07-28 10:48:18 +0100

    updates

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Roroacms
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.assets.initialize_on_precompile = false
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
