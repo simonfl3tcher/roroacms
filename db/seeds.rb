f0ac55d978ac7179578e80844fa2c1f201e8c810
Simon Fletcher <Macbook>
2013-07-28 09:55:47 +0100

    heroku check

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+APP_CONFIG = '123123'
+
+
+
 
 Setting.create :setting_name => 'articles_slug', :type_of_setting => 'G', :setting => ENV['articles_slug']
 Setting.create :setting_name => 'home_page', :type_of_setting => 'G', :setting => '1'
