88a214e99e0d98790f28acd105613eec6f79db99
Simon Fletcher <Macbook>
2014-04-21 12:21:30 +0100

    All Controllers commented up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -28,26 +28,20 @@ class PagesController < ApplicationController
 	# save and send and email with the details to the admin email address that is set in the admin panel
 
 	def contact_form
-
 		@message = ContactForm.new(contact_form_params)
 		session[:return_to] = request.referer
 		
 		respond_to do |format|
 		  
 		  if @message.save
-
 		  	EmailContactFormWorker.perform_async(@message.id)
 		    format.html { redirect_to "#{session[:return_to]}", notice: "Your message has been sent successfully thank you." }
-
 		  else
-
 		  	flash[:error] = 'You missed out some required fields. Please try again.'
 		    format.html { redirect_to "#{session[:return_to]}" }
-
 		  end
 		  
 		end
-
 	end
 
 end
