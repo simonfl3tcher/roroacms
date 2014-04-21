8862a419028a7594c4551109de356dbc46dc707e
Simon Fletcher <Macbook>
2014-04-21 13:04:41 +0100

    Comment model commented up

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,35 +2,40 @@ class Comment < ActiveRecord::Base
 
 	has_ancestry
 	
+	# relations and validations
+
 	belongs_to :post
-
 	validates :email, presence: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }
-
 	validates :author, :comment, :presence => true
+
+	# set defaults before comment gets added to the database 
 
 	before_create :set_defaults
 
-	def self.setup_and_search(params)
+	# get all the comments in the system - however if there is a search parameter 
+	# search the necessary fields for the given value
 
-		@comments = Comment.where("comment_approved != 'S'").order('submitted_on desc').page(params[:page]).per(Setting.get_pagination_limit)
-		
+	def self.setup_and_search(params)
 		if params.has_key?(:search) && !params[:search].blank?
-			@comments = Comment.where("(author like ? or email like ? or comment like ?) and comment_approved != 'S'", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order('submitted_on desc').page(params[:page]).per(Setting.get_pagination_limit)
+			comments = Comment.where("(author like ? or email like ? or comment like ?) and comment_approved != 'S'", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").order('submitted_on desc').page(params[:page]).per(Setting.get_pagination_limit)
+		else
+			comments = Comment.where("comment_approved != 'S'").order('submitted_on desc').page(params[:page]).per(Setting.get_pagination_limit)
 		end
 
 		if params.has_key?(:filter) && params[:filter] != 'filter'
-			@comments = Comment.where(:comment_approved => params[:filter]).order('created_at desc').page(params[:page]).per(Setting.get_pagination_limit)
+			comments = Comment.where(:comment_approved => params[:filter]).order('created_at desc').page(params[:page]).per(Setting.get_pagination_limit)
 		end
 
-		return @comments
-
+		return comments
 	end
+
+	# The bootstrap for the bulk update function. It takes in the call
+	# and decides what function to call in order to get the correct output
 
 	def self.bulk_update(params)
 
 		action = params[:to_do]
 		action = action.gsub(' ', '_')
-
 
 		case action.downcase 
 			when "unapprove"
@@ -52,17 +57,20 @@ class Comment < ActiveRecord::Base
 
 	private 
 
+	# set default values of the record before adding to the database
+
 	def set_defaults
-		
 		self.comment_approved = 'N'
 		self.submitted_on = Time.now.to_s(:db) 
-
 	end
 
+	# bulk unapprove given comments
 
 	def self.bulk_update_unapprove(comments)
 		Comment.update(comments, comment_approved: 'N')
 	end
+
+	# bulk approve given comments
 	
 	def self.bulk_update_approve(comments)
 		comments.each do |val|
@@ -70,8 +78,9 @@ class Comment < ActiveRecord::Base
 			comment.comment_approved = "Y"
 			comment.save
 		end
-
 	end
+
+	# bulk mark as spam for given comments
 	
 	def self.bulk_update_mark_as_spam(comments)
 		comments.each do |val|
@@ -79,23 +88,15 @@ class Comment < ActiveRecord::Base
 			comment.comment_approved = "S"
 			comment.save
 		end
-
 	end
+
+	# bulk delete given comments
 	
 	def self.bulk_update_destroy(comments)
 		comments.each do |val|
 			comment = Comment.find(val)
 			comment.destroy
 		end
-	end
-
-
-	def self.multiple_update ids, attribute
-
-		abort ids.to_i.inspect
-
-  		Comment.update(ids, attribute)
-		
 	end
   
 end
