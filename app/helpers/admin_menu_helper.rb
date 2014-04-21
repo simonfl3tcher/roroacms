1fb312897d4cdba8b4224ca2f8685dcbe9489bc9
Simon Fletcher <Macbook>
2014-04-21 21:35:10 +0100

    Helpers half commented

--- app/helpers/admin_menu_helper.rb
+++ app/helpers/admin_menu_helper.rb
@@ -1,23 +1,27 @@
 module AdminMenuHelper
 
+	# Returns a hash as key and value from the custom data string
+	# Params:
+	# +str+:: contains all of the data for the link
 
 	def make_hash(str)
 
 		str = str.split('&')
-
 		hash = {}
 
-		str.each do | s |
-
+		str.each do |s|
 			opt = s.split('=')
-
 			hash[opt[0]] = URI.unescape(opt[1].to_s.gsub('+', ' '))
-
 		end
 
-		return hash
+		hash
 
 	end
+
+	# Returns a string to check the checkbox if the two variables match
+	# Params:
+	# +one+:: comparable to two
+	# +two+:: comparable to one
 
 	def check_for_checked(one, two)
 		if one.to_s == two.to_s
@@ -27,7 +31,14 @@ module AdminMenuHelper
 		end
 	end
 
+	# Returns the menu options in a html format for the admin editing area
+	# Params:
+	# +menuid+:: the id from the "menus" record
+	# +sub+:: is true if it is a sub menu
+
 	def menu_list_loop(menuid, sub = false)
+
+		# check to see if it is a sub menu 
 		if sub 
 			data = MenuOption.find(menuid)
 			data = MenuOption.where(:parent_id => data.option_id)
@@ -38,9 +49,11 @@ module AdminMenuHelper
 		html = ''
 
 		data.each do |m|
-			if m.parent_id != 0
-				existingData = make_hash m.custom_data
 
+			if m.parent_id != 0
+
+				# below creates the necessary vairbale for the html view
+				existingData = make_hash m.custom_data
 				name = existingData['label']
 
 				if name.to_s.blank?
@@ -56,30 +69,37 @@ module AdminMenuHelper
 					end
 				end
 
+				# allow the view to see these variables
 				@option = m
 				@existingData = existingData
 				@name = name
 
+				# add each block to the html variable
 				html += render :partial => 'admin/helper_partials/menu_list_loop' 
 
 			end
 		end
 
-		return html.html_safe	
+		html.html_safe	
 
 	end
 
+	# returns the count of the children within the lft and rgt values
+	# Params:
+	# +value+:: the current record that you want to check against
 	
-	def descendants_count(m)
-      return (m.rgt - m.lft - 1)/2
+	def descendants_count(value)
+      return (value.rgt - value.lft - 1)/2
     end
+
+    # returns a list of all of the menus as an accordion with its list items inside 
+	# Params:
+	# +menu+:: the current record that you want to create the menu group option for
 	
 	def get_menu_groups(menu)
-
 		@id = menu.name.gsub(' ', '');
 		@menu = menu
     	return render :partial => 'admin/helper_partials/menu_menu_groups' 
-
 	end
 	
 
--- app/helpers/admin_roroa_helper.rb
+++ app/helpers/admin_roroa_helper.rb
@@ -1,133 +1,143 @@
 module AdminRoroaHelper
 
+	# returns wether it is numeric or not
+	# Params:
+	# +obj+:: either an int, float, decimal or string of a number or not depending on what you are looking for
+
 	def is_numeric?(obj) 
 	   obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
 	end
 
+	# returns a hash of the different themes as a hash with the details kept in the theme.yml file
+
 	def get_theme_options
+		
 		hash = []
 		Dir.glob("app/views/theme/*/") do |themes|
 			opt = themes.split('/').last
 			info = YAML.load(File.read("#{themes}theme.yml"))
 			hash << info
 		end
-		return hash
+
+		hash
 	end
 
-	def destory_theme(id)
+	# destroys the theme from the file structure
+	# Params:
+	# +theme+:: the theme name which is set in the theme.yml file and should be the same as the folder name
+
+	def destory_theme(theme)
 		require 'fileutils'
-		FileUtils.rm_rf("app/views/theme/#{id}")
+		FileUtils.rm_rf("app/views/theme/#{theme}")
 	end
+
+	# retuns a hash of the template files within the current theme
 
 	def get_templates
 		hash = []
-
 		current_theme = Setting.find_by_setting_name('theme_folder')[:setting]
 
 		Dir.glob("app/views/theme/#{current_theme}/template-*.html.erb") do |my_text_file|
 			opt = my_text_file.split('/').last
 			opt['template-'] = ''
 			opt['.html.erb'] = ''
+			# strips out the template- and .html.erb extention and returns the middle as the template name for the admin
 			hash << opt.titleize
 		end
 
-		return hash
+		hash
 	end
 
-	def get_template_dropdown(cur)
+	# returns a html dropdown of the template options 
+	# Params:
+	# +current+:: current post template, if it doesn't have one it will just return a standard dropdown
+
+	def get_template_dropdown(current = '')
 
 		templates = get_templates
-		str =''
+		str = ''
+
 		templates.each do |f|
-			if f == cur
+			if f == current
 				str	+= "<option value='#{f}' selected='selected'>#{f}</option>"
 			else
 				str	+= "<option value='#{f}'>#{f}</option>"
 			end
-
 		end
 
-		return str.html_safe
+		str.html_safe
 
 	end
 
-	def upload_file(file)
+	# returns boolean as to wether the logged in user is the complete overlord of the system
 
-		File.open(Rails.root.join('public', 'uploads', file.original_filename), 'w') do |file|
-			file.write(file.read)
-		end	
-
-	end
-
-	def is_overlord
-
+	def is_overlord?
 		admin = Admin.find(session[:admin_id])
-
-		if admin.overlord == 'Y'
-
-			true
-
-		else 	
-
-			false
-
-		end
-
-
+		admin.overlord == 'Y' ? true : false
 	end
+
+	# returns a table block of html that has nested ootions
+	# Params:
+	# +options+:: is a hash of all of the record that you want to display in a nested table
 
 	def nested_table(options)
-	  
-	  options.map do |opt, sub_messages|
-	  	@content = opt
-	  	@sub = sub_messages
-	    render('admin/partials/table_row')
-	  end.join.html_safe
+		options.map do |opt, sub_messages|
+			@content = opt
+			# set the sub to be sub messages. The view checks its not blank and runs this function again 
+			@sub = sub_messages
 
+			render('admin/partials/table_row')
+
+		end.join.html_safe
 	end
+
+	# returns a html block of line indentation to show that it is underneath its parent
+	# Params:
+	# +cont+:: a hash of the record you are checking against
 
 	def ancestory_indent(cont)
 
+		html = ''
 		cont.ancestor_ids.length
 
 		i = 0
-
-		html = ''
 		while i < cont.ancestor_ids.length  do
 		   html += "<i class=\"icon-minus\"></i>"
-		   i +=1
+		   i += 1
 		end
 
 		render :inline =>  html.html_safe
 
 	end
 
-	def site_url(url )
+	# returns the site url + an extention if you give it one
+	# Params:
+	# +ext+:: extension to add on to the end of the site url
 
-		if url 
+	def site_url(ext)
+		
+		# if you do not give ext anything it will just return the standard site url
 
+		if ext 
 			base_url = Setting.get('site_url')	
-
 			return "#{base_url}#{url}"
-
 		else 
-
 			return Setting.get('site_url')		
-
 		end 
 
 	end 
 
+	# checks if the current theme being used actually exists. If not it will return an error message to the user
+
 	def theme_exists
+		
 		current_theme = Setting.find_by_setting_name('theme_folder')[:setting]
 
-
 		if !Dir.exists?("app/views/theme/#{current_theme}/")
-
-			html = "<div class='alert alert-danger'><strong>Warning!</strong> The current theme does not exist!</div>"
+			html = "<div class='alert alert-danger'><strong>Warning!</strong> The current theme being used does not exist!</div>"
 	    	render :inline => html.html_safe
-
 		end
 
 	end
+
 end
