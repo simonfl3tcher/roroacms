563bc9943eeab94ec324327f849ff8217cdbcdd6
Simon Fletcher <Macbook>
2014-04-21 18:48:05 +0100

    admin menu helper

--- app/helpers/admin_media_helper.rb
+++ app/helpers/admin_media_helper.rb
@@ -1,31 +1,21 @@
 module AdminMediaHelper
 
-	AWS::S3::Base.establish_connection!(
-      :access_key_id     => Setting.find_by_setting_name('aws_access_key_id')[:setting],
-      :secret_access_key => Setting.find_by_setting_name('aws_secret_access_key')[:setting]
-    )
+	# establish a connection to S3
 
+	AWS::S3::Base.establish_connection!(:access_key_id => Setting.find_by_setting_name('aws_access_key_id')[:setting], :secret_access_key => Setting.find_by_setting_name('aws_secret_access_key')[:setting])
 	BUCKET = Setting.find_by_setting_name('aws_bucket_name')[:setting]
 
-	def download_url_for(song_key)  
-	    AWS::S3::S3Object.url_for(song_key, BUCKET, :authenticated => false)  
+	# Returns a url to the file from S3
+	# Params:
+	# +key+:: string to the file 
+
+	def download_url_for(key)  
+	    AWS::S3::S3Object.url_for(key, BUCKET, :authenticated => false)  
 	end
 
-	def torrent_url_for(song_key)  
-	    download_url_for(song_key) + "?torrent"  
-	end  
-
-	def get_bucket_size
-
-		size = 0
-
-		AWS::S3::Bucket.find(BUCKET).objects.each do |object| #object should be an S3Object
-		    size += object.content_length.to_i #in bytes
-		end
-
-		return number_to_human_size(size)
-
-	end
+	# Returns a block of html 
+	# Params:
+	# +initial+:: is the initial iteration true/false
 
 	def render_folder_loop(initial = false)
 
@@ -33,41 +23,60 @@ module AdminMediaHelper
 
 		@initial = initial
 		@f = Array.new
- 		files.each_with_index {|item, index|
+ 		
+ 		files.each_with_index do |item, index|
  			if item.content_type != 'binary/octet-stream'
 				next
 			end
 
 		   @f.push item
-		}
+		end
+		# render the html
 		render :partial => 'admin/media/media_folder_loop'
 
-
 	end
+
+	# Create a folder tree of the directories on S3. 
+	# This is complicated as S3 returns the data as a string.
+	# Params:
+	# +fulldata+:: array of all of the data
+	# +nextalong+:: the next data along
+	# +i+:: counter which has been set from the front end html
 
 	def make_media_folder_subpage(fulldata, nextalong, i, y = false)
 
 		hasFolder = false
+		
 		if !fulldata[i+2].blank?
 			if fulldata[i+1].key.count('/') < fulldata[i+2].key.count('/') 
+				# check to see if it has a folder. This is so the system knows wether to 
+				# make another iteration
 				hasFolder = true
 			end
 		end
 
 		file = fulldata[i+1]
 		name = file.key.split("/").last
+		
 		if !y
 			html = '<ul>'
 		end
+		
+		# add li for the folder
 		html += "<li class='folderRow'>
-						<a class='folderLink' href='' data-key='#{file.key}' data-hasfolder='#{hasFolder}'>
+<a class='folderLink' href='' data-key='#{file.key}' data-hasfolder='#{hasFolder}'>
 <i class='icon-folder-close'></i>&nbsp;<span>#{name}</span>
-						<i class='icon-remove'></i></a>"
+<i class='icon-remove'></i></a>"
+	
+		# if full data plus two is not blank make another iteration
 		if !fulldata[i+2].blank?
+			
 			if fulldata[i+1].key.count('/') < fulldata[i+2].key.count('/') 
 				html += make_media_folder_subpage fulldata, fulldata[i+1], i+1
 			end
+
 		end
+
 		html += "</li>"
 
 		if !y
@@ -75,5 +84,7 @@ module AdminMediaHelper
 		end
 
 		return html.html_safe
+
 	end
+
 end
