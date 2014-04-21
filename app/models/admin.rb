11b747c2aed247926b0a5f38aefc4c6775f22a87
Simon Fletcher <Macbook>
2014-04-21 12:45:00 +0100

    Admin model commented up

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,37 +1,39 @@
 class Admin < ActiveRecord::Base
 
 	has_secure_password
+	
+	# relations and validations
 
 	has_many :posts
-	
 	validates :email, presence: true, uniqueness: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
-
   	validates :username, :access_level, :presence => true 
-
-	ACCESS_LEVELS = ["admin", "editor"]
-
 	validates_uniqueness_of :email, :username
-
 	validates_presence_of :password, :on => :create
 
+	# general data mainly for views
+
+	ACCESS_LEVELS = ["admin", "editor"]
 	GET_ADMINS = Admin.where('1=1').order('name asc')
 
-	def self.setup_and_search(params)
 
-		admins = Admin.where('1+1=2').page(params[:page]).per(Setting.get_pagination_limit)
+	# get all the admins in the system - however if there is a search parameter 
+	# search the necessary fields for the given value
+
+	def self.setup_and_search(params)
 		if params.has_key?(:search)
 			admins = Admin.where("email like ? or first_name like ? or last_name like ? or username like ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").page(params[:page]).per(Setting.get_pagination_limit)
+		else 
+			admins = Admin.where('1+1=2').page(params[:page]).per(Setting.get_pagination_limit)
 		end
-
+		
 		return admins
-
 	end
 
-	def self.set_sessions(session, admin)
+	# set the session data for the admin to allow/restrict the necessary areas
 
+	def self.set_sessions(session, admin)
 		session[:admin_id] = admin.id
 		session[:username] = admin.username
-
 	end
 
 	def self.set_sessions_manually(session, i, name)
@@ -39,27 +41,12 @@ class Admin < ActiveRecord::Base
 		session[:username] = name
 	end
 
-	def self.destroy_session(session)
 
+	# destroy the session data - logging them out of the admin panel
+
+	def self.destroy_session(session)
 		session[:admin_id] = nil
 		session[:username] = nil
-
-	end
-
-	def self.post_status_sql(session)
-
-		if !session[:admin_id].blank?
-
-			status = "(post_status = 'Published')"
-
-		else
-
-			status = "(post_status = 'Published' AND post_date <= DATE(NOW()))"
-
-		end
-
-		return status
-
 	end
 
 end 
