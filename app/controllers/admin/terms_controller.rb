58f64314bf8ea391908bfd827ece373384027a2b
Simon Fletcher <Macbook>
2014-05-26 11:51:38 +0100

    categories, tag and comments

--- app/controllers/admin/terms_controller.rb
+++ app/controllers/admin/terms_controller.rb
@@ -3,16 +3,32 @@ class Admin::TermsController < AdminController
 	# displays all the current categories and creates a new category object for creating a new one
 
 	def categories
-		@categories = Term.where(term_anatomies: {taxonomy: 'category'}).order('name asc').includes(:term_anatomy).page(params[:page]).per(Setting.get_pagination_limit)
+		# add breadcrumb and set title
+		add_breadcrumb "Categories", :admin_post_categories_path, :title => "Back to categories"
+		@title = "Categories"
+
+		@terms = Term.where(term_anatomies: {taxonomy: 'category'}).order('name asc').includes(:term_anatomy).page(params[:page]).per(Setting.get_pagination_limit)
 		@category = Term.new
+		@type = 'category'
+
+		# render view template as it is the same as the tag view
+		render 'view'
 	end
 
 
 	# displays all the current tags and creates a new category object for creating a new one
 
 	def tags
-		@tags = Term.where(term_anatomies: {taxonomy: 'tag'}).order('name asc').includes(:term_anatomy).page(params[:page]).per(Setting.get_pagination_limit)
+		# add breadcrumb and set title
+		add_breadcrumb "Tags", :admin_post_tags_path, :title => "Back to tags"
+		@title = "Tags"
+
+		@terms = Term.where(term_anatomies: {taxonomy: 'tag'}).order('name asc').includes(:term_anatomy).page(params[:page]).per(Setting.get_pagination_limit)
 		@category = Term.new
+		@type = 'tag'
+
+		# render view template as it is the same as the category view
+		render 'view'
 	end
 
 
@@ -31,11 +47,16 @@ class Admin::TermsController < AdminController
 		respond_to do |format|
 
 		  if @category.save
+
 		  	@term_anatomy = @category.create_term_anatomy(:taxonomy => params[:type_taxonomy])
+		  	Term.update_slug_for_subcategories(@category.id, @category.structured_url, true)
 		    format.html { redirect_to send(redirect_url), notice: "#{type} was successfully created." }
+
 		  else
+
 		  	flash[:error] = 'Please make sure the slug is unique.'
 		    format.html { redirect_to send(redirect_url) }
+
 		  end
 
 		end
@@ -46,7 +67,19 @@ class Admin::TermsController < AdminController
 
 	def edit 
 		@category = Term.find(params[:id])
+
+		if @category.term_anatomy.taxonomy == 'category' 
+			add_breadcrumb "Categories", :admin_post_categories_path, :title => "Back to categories"
+			add_breadcrumb "Article Category"
+			@title = 'Article Category'
+		else
+			add_breadcrumb "Tags", :admin_post_tags_path, :title => "Back to tags"
+			add_breadcrumb "Article Tag"
+			@title = 'Article Tag'
+		end
+
 		@type = @category.term_anatomy.taxonomy
+
 	end
 
 
@@ -63,14 +96,10 @@ class Admin::TermsController < AdminController
 	    respond_to do |format|
 	    	# deal with abnormalaties - update the structure url 
 			if @category.update_attributes(term_params)
-
 			    Term.update_slug_for_subcategories(@category.id, old_url, true)
-				format.html { redirect_to send(redirect_url), notice: "#{type} was successfully updated" }
-
+				format.html { redirect_to edit_admin_term_path(@category), notice: "#{type} was successfully updated" }
 			else
-
 				format.html { render action: "edit" }
-
 			end
 
 	    end
