c99ebe8f35ad4d7acdcd333c01af1da9f8d75ad7
Simon Fletcher <simonfletcher0@gmail.com>
2014-07-02 18:24:12 +0100

    spec added

--- spec/requests/admin/trash_spec.rb
+++ spec/requests/admin/trash_spec.rb
@@ -5,4 +5,51 @@ RSpec.describe "Admin::Trash", :type => :request do
 	let(:admin) { FactoryGirl.create(:admin) }
   	before { sign_in(admin) }
 
+	describe "GET /admin/administrators" do
+
+		context "without posts or pages in the trash can" do 
+			expect(page).to have_content('The trash can is currently empty')
+		end
+
+		context "with posts or pages in the trash can" do 
+
+			before(:each) do 
+				let!(:post) { FactoryGirl.create(:post, disabled: 'Y', post_type: 'post') }
+				let!(:page) { FactoryGirl.create(:post, disabled: 'Y', post_type: 'page') }
+				visit admin_trash_path
+			end
+
+			it "should display trash" do 
+				expect(page).to have_content "Trash"
+			end
+
+			it "should have have post on view" do 
+				expect(page).to have_content post.title
+			end
+
+			it "should have page on view" do 
+				expect(page).to have_content page.title
+			end
+
+			it "should delete all records" do 
+				# this html find function needs cleaning up
+				find('page #delete_all').click
+				expect(current_path).to eq(admin_trash_path)
+				expect(page).to have_content('')
+			end
+
+			it "should delete certian records" do 
+				
+			end
+
+			it "should reinstate certain records" do 
+				first('reinstate').click
+				expect(current_path).to eq(admin_trash_path)
+				expect(page).to have_content('These records was successfully reinstated')
+			end
+
+		end
+
+	end
+
 end
