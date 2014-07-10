ce1bb7b7aecd9dd3ff405b33b6f2e7025204a3b5
Simon Fletcher <simonfletcher0@gmail.com>
2014-07-10 18:21:14 +0100

    Specs added for views

---
+++ spec/helpers/new_view_helper_spec.rb
@@ -0,0 +1,251 @@
+require "spec_helper"
+
+describe NewViewHelper do
+
+	describe "obtain_children" do 
+		it "should return the given posts children"
+	end
+
+	describe "#obtain_permalink" do 
+		it "should return the post url"
+	end
+
+	describe "#obtain_excerpt" do 
+		it "should return an excerpt of the post"
+	end
+
+	describe "#obtain_cover_image" do 
+		it "should return the cover image"
+	end
+
+	describe "#obtain_the_date" do 
+		it "should return the published date of the post/page"
+	end
+
+	describe "#has_cover_image" do 
+		it "should return true if it has a cover image"
+		it "should return false if it does not have a cover image"
+	end
+
+	describe "#obtain_all_category_ids" do 
+		it "should return all category ids"
+		it "should return all category ids of the categories the post is in"
+	end
+
+	describe "#obtain_categories" do 
+		it "should return all the categories"
+	end
+
+	describe "#obtain_category" do 
+		it "should return the category"
+	end
+
+	describe "#obtain_category_title" do 
+		it "should return the category title"
+	end
+
+	describe "#obtain_category_link" do 
+		it "should return the category link"
+	end
+
+	describe "#obtain_category_description" do 
+		it "should return the category description"
+	end
+
+	describe "#in_category" do 
+		it "should return a bool as to wether the given post is in the given category"
+	end
+
+	describe "#obtain_tag_link" do 
+		it "should return the tag url"
+	end
+
+	describe "#obtain_tags" do 
+		it "should return all the categories"
+	end
+
+	describe "#has_tag" do 
+		it "should return wether the post has the given tag attached"
+	end
+
+	describe "#obtain_tag_title" do 
+		it "should return the tag title"
+	end
+
+	describe "#obtain_tag_description" do 
+		it "should return the tag description"
+	end
+
+	describe "#obtain_tag" do 
+		it "should return the tag"
+	end
+
+	describe "#obtain_next_article" do 
+		it "should return the article that is next in line"
+	end
+
+	describe "#obtain_next_article_link" do 
+
+	end
+
+	describe "#obtain_previous_article" do 
+
+	end
+
+	describe "#obtain_previous_article_link" do 
+
+	end
+
+	describe "#obtain_article" do 
+
+	end
+
+	describe "#obtain_article_field" do 
+
+	end
+
+	describe "#obtain_article_status" do 
+
+	end
+
+	describe "#obtain_articles" do 
+
+	end
+
+	describe "#obtain_archives" do 
+
+	end
+
+	describe "#obtain_page" do 
+
+	end
+
+	describe "#obtain_page_link" do 
+
+	end
+
+	describe "#obtain_page_uri" do 
+
+	end
+
+	describe "#obtain_page_field" do 
+
+	end
+
+	describe "#is_page" do 
+
+	end
+
+	describe "#is_archive" do 
+
+	end
+
+	describe "#is_articles_home" do 
+
+	end
+
+	describe "#is_day_archive" do 
+
+	end
+
+	describe "#is_month_archive" do 
+
+	end
+
+	describe "#is_year_archive" do 
+
+	end
+
+	describe "#obtain_archive_year" do 
+
+	end
+
+	describe "#is_homepage" do 
+
+	end
+
+	describe "#is_article" do 
+
+	end
+
+	describe "#is_search" do 
+
+	end
+
+	describe "#is_tag" do 
+
+	end
+
+	describe "#is_category" do 
+
+	end
+
+	describe "#obtain_user_profile" do 
+
+	end
+
+	describe "#obtain_users" do 
+
+	end
+
+	describe "#obtain_user_field" do 
+
+	end
+
+	describe "#obtain_comment_author" do 
+
+	end
+
+	describe "#obtain_comment_date" do 
+
+	end
+
+	describe "#obtain_comment_time" do 
+
+	end
+
+	describe "#obtain_comments" do 
+
+	end
+
+	describe "#obtain_comments_form" do 
+
+	end
+
+	describe "#obtain_id" do 
+
+	end
+
+	describe "#obtain_the_author" do 
+
+	end
+
+	describe "#obtain_the_authors_articles" do 
+
+	end
+
+	describe "#obtain_the_content" do 
+
+	end
+
+	describe "#obtain_the_title" do 
+
+	end
+
+	describe "#obtain_term_type" do 
+
+	end
+
+	describe "#obtain_additional_data" do 
+
+	end
+
+	describe "#create_excerpt" do 
+
+	end
+
+	describe "#obtain_record" do 
+
+	end
+
+end
